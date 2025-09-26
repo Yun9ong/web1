fcd57b7ff14ade84ac28cb5772d97d90c65725f0
LeeWeb <lee456624@gmail.com>
2025-09-26 01:19:52 +0000

    feat: 优化游戏中心并适配移动端

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
+// src/main.js
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
-const app = createApp(App)
-
-// 将 SpeedInsights 注册为全局组件
-app.component('SpeedInsights', SpeedInsights)
-
-app.mount('#app')
-
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)   // 注册路由
+  .mount('#app') // 挂载节点
